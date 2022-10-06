ee4af892865548e3e8b90adb32746af9a25a6c0d
jeiobaco <jeisonobando1333@gmail.com>
2022-10-06 15:23:55 -0500

    finish detail blog with project

--- src/app/modules/projects/vivienda/vivienda.module.ts
+++ src/app/modules/projects/vivienda/vivienda.module.ts
@@ -5,6 +5,7 @@ import { ViviendaRoutingModule } from './vivienda-routing.module';
 import { ViviendaComponent } from './pages/vivienda/vivienda.component';
 import { ComponentSharedModule } from 'src/app/shared/components/components.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BreadcrumbComponent } from 'src/app/shared/components/breadcrumb/breadcrumb.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     ViviendaRoutingModule,
     ComponentSharedModule,
-    HttpClientModule
+    HttpClientModule,
+    BreadcrumbComponent
   ]
 })
 export class ViviendaModule { }
