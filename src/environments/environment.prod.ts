ef7a53e9742e801f961225a11478f10bc244f6c2
Eliecer <eliecer.vasquez@paxzu.co>
2022-10-10 08:56:51 -0500

    ajuste administrador

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  base_url: 'https://adminpaxzu-devcons.oikos.com.co/',
-  api_url: 'https://adminpaxzu-devcons.oikos.com.co/api/', //establecer el subdominio para esta sección
+  base_url: 'https://adminpaxzu-cons.oikos.com.co/',
+  api_url: 'https://adminpaxzu-cons.oikos.com.co/api/', //establecer el subdominio para esta sección
   token: 'aff7d877-d0ff-4dfa-a85c-d9715ea82061', //se genera al publicar,
-  imagenes_url: 'https://adminpaxzu-devcons.oikos.com.co/', //establecer el subdominio para esta sección
+  imagenes_url: 'https://adminpaxzu-cons.oikos.com.co/', //establecer el subdominio para esta sección
 };
