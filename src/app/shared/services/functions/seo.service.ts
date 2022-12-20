7040eba8057d8e3343e6c559911c8f57c6762ea8
Eliecer <eliecer.vasquez@paxzu.co>
2022-12-20 12:48:51 -0500

    ajuste routing 15

--- src/app/shared/services/functions/seo.service.ts
+++ src/app/shared/services/functions/seo.service.ts
@@ -36,7 +36,7 @@ export class SeoService {
       this.updateCanonicalUrl(canonical);
       this.titlePage.setTitle(seoPage.title)
       this.meta.updateTag({ name: 'description', content: seoPage.metadescription });
-      this.meta.updateTag({ name: 'robots', content: seoPage.metarobots });
+      //this.meta.updateTag({ name: 'robots', content: seoPage.metarobots });
     }
   }
 }
