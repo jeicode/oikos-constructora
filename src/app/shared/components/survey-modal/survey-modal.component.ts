30e5439eba2b5372d171c02e6cf137729ce9b119
Cristian <cristian.garcia@paxzu.co>
2023-05-12 08:36:33 -0500

    maqueta modal encuesta

---
+++ src/app/shared/components/survey-modal/survey-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'survey-modal',
+    templateUrl: './survey-modal.component.html',
+    styleUrls: ['./survey-modal.component.css']
+
+})
+
+export class surveyModalComponent {
+    
+}
