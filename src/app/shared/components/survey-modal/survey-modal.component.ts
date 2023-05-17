e94c28d947bf67a4dd95df2db07c2e81c3f671fc
evasquezg1 <eliecervasquez94@hotmail.com>
2023-05-17 11:52:33 -0500

    ajuste avance encuesta

--- src/app/shared/components/survey-modal/survey-modal.component.ts
+++ src/app/shared/components/survey-modal/survey-modal.component.ts
@@ -1,13 +1,26 @@
 import { Component } from "@angular/core";
 
+declare var $:any;
 @Component({
 
     selector: 'survey-modal',
     templateUrl: './survey-modal.component.html',
     styleUrls: ['./survey-modal.component.css']
-
 })
 
 export class surveyModalComponent {
     
+    activarEncuestaS             : boolean = false;
+
+    constructor(){}
+
+    activarEncuesta(){
+        this.activarEncuestaS = !this.activarEncuestaS;
+        this.avanzar('portada');
+    }
+
+    avanzar(paso: any){
+        $(".cnt_sec_modal").removeClass('active');
+        $("."+paso).addClass('active');
+    }
 }
