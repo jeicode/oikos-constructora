fce204a2bf3a0d8c4c1bbb65656e2f8601f8e0f9
Jeison Obando <jeison.obando@paxzu.co>
2024-03-05 13:28:23 -0500

    banner home testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,50 +7,50 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
   }, 
-  // {
-  //   path: 'contactanos',
-  //   loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
-  // }, 
-  // {
-  //   path: 'gracias/:slug',
-  //   loadChildren: () => import('./modules/thanks/thanks.module').then(m => m.ThanksModule)
-  // }, 
-  // {
-  //   path: 'noticias-constructora',
-  //   loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule)
-  // },
-  // {
-  //   path: 'proyectos-construccion-vivienda',
-  //   loadChildren: () => import('./modules/projects/vivienda/vivienda.module').then(m => m.ViviendaModule)
-  // },
-  // {
-  //   path: 'proyectos-construccion-comerciales-industriales',
-  //   loadChildren: () => import('./modules/projects/comerciales/comerciales.module').then( m => m.ComercialesModule )
-  // },
-  // {
-  //   path: 'proyecto/:slug',
-  //   loadChildren: () => import('./modules/projects/interna/interna.module').then(m => m.InternaModule)
-  // },
-  // {
-  //   path: 'legales',
-  //   loadChildren: () => import('./modules/legales/legales.module').then( m => m.LegalesModule )
-  // },{
-  //   path: 'proyectos-ejecutados',
-  //   loadChildren: () => import('./modules/projects/ejecutados/ejecutados.module').then( m => m.EjecutadosModule )
-  // },{
-  //   path: 'pagos-constructora',
-  //   loadChildren: () => import('./modules/pagos/pagos.module').then( m => m.PagosModule )
-  // },{
-  //   path: 'mapa-del-sitio',
-  //   loadChildren: () => import('./modules/mapa/mapa.module').then( m => m.MapaModule )
-  // },{
-  //   path: '**',
-  //   component: NotFoundPageComponent
-  // }, 
-  // {
-  //   path: '404',
-  //   component: NotFoundPageComponent
-  // }
+  {
+    path: 'contactanos',
+    loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
+  }, 
+  {
+    path: 'gracias/:slug',
+    loadChildren: () => import('./modules/thanks/thanks.module').then(m => m.ThanksModule)
+  }, 
+  {
+    path: 'noticias-constructora',
+    loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule)
+  },
+  {
+    path: 'proyectos-construccion-vivienda',
+    loadChildren: () => import('./modules/projects/vivienda/vivienda.module').then(m => m.ViviendaModule)
+  },
+  {
+    path: 'proyectos-construccion-comerciales-industriales',
+    loadChildren: () => import('./modules/projects/comerciales/comerciales.module').then( m => m.ComercialesModule )
+  },
+  {
+    path: 'proyecto/:slug',
+    loadChildren: () => import('./modules/projects/interna/interna.module').then(m => m.InternaModule)
+  },
+  {
+    path: 'legales',
+    loadChildren: () => import('./modules/legales/legales.module').then( m => m.LegalesModule )
+  },{
+    path: 'proyectos-ejecutados',
+    loadChildren: () => import('./modules/projects/ejecutados/ejecutados.module').then( m => m.EjecutadosModule )
+  },{
+    path: 'pagos-constructora',
+    loadChildren: () => import('./modules/pagos/pagos.module').then( m => m.PagosModule )
+  },{
+    path: 'mapa-del-sitio',
+    loadChildren: () => import('./modules/mapa/mapa.module').then( m => m.MapaModule )
+  },{
+    path: '**',
+    component: NotFoundPageComponent
+  }, 
+  {
+    path: '404',
+    component: NotFoundPageComponent
+  }
 ];
 
 @NgModule({
