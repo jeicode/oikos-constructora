213d2bd303043de1503f8a5e343cbfcc98d6eb75
Jeison Obando <jeison.obando@paxzu.co>
2024-03-20 15:49:12 -0500

    add routing module. STEP2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,56 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { homeGuard } from './core/guards/home.guard';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadComponent: () => import('./modules/home/home-page.component').then( m => m.HomePageComponent),
-  //   canActivate: [homeGuard]
-  // }, 
-  // {
-  //   path: 'contactanos',
-  //   loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
-  // }, 
-  // {
-  //   path: 'gracias/:slug',
-  //   loadChildren: () => import('./modules/thanks/thanks.module').then(m => m.ThanksModule)
-  // }, 
-  // {
-  //   path: 'noticias-constructora',
-  //   loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule)
-  // },
-  // {
-  //   path: 'proyectos-construccion-vivienda',
-  //   loadChildren: () => import('./modules/projects/vivienda/vivienda.module').then(m => m.ViviendaModule)
-  // },
-  // {
-  //   path: 'proyectos-construccion-comerciales-industriales',
-  //   loadChildren: () => import('./modules/projects/comerciales/comerciales.module').then( m => m.ComercialesModule )
-  // },
-  // {
-  //   path: 'proyecto/:slug',
-  //   loadChildren: () => import('./modules/projects/interna/interna.module').then(m => m.InternaModule)
-  // },
-  // {
-  //   path: 'legales',
-  //   loadChildren: () => import('./modules/legales/legales.module').then( m => m.LegalesModule )
-  // },{
-  //   path: 'proyectos-ejecutados',
-  //   loadChildren: () => import('./modules/projects/ejecutados/ejecutados.module').then( m => m.EjecutadosModule )
-  // },{
-  //   path: 'pagos-constructora',
-  //   loadChildren: () => import('./modules/pagos/pagos.module').then( m => m.PagosModule )
-  // },{
-  //   path: 'mapa-del-sitio',
-  //   loadChildren: () => import('./modules/mapa/mapa.module').then( m => m.MapaModule )
-  // },{
-  //   path: '**',
-  //   loadComponent: () => import('../app/shared/pages/not-found-page/not-found-page.component').then( m => m.NotFoundPageComponent),
-  // }, 
-  // {
-  //   path: '404',
-  //   loadComponent: () => import('../app/shared/pages/not-found-page/not-found-page.component').then( m => m.NotFoundPageComponent),
-  // }
+  {
+    path: '',
+    loadComponent: () => import('./modules/home/home-page.component').then( m => m.HomePageComponent),
+    canActivate: [homeGuard]
+  }, 
+  {
+    path: 'contactanos',
+    loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
+  }, 
+  {
+    path: 'gracias/:slug',
+    loadChildren: () => import('./modules/thanks/thanks.module').then(m => m.ThanksModule)
+  }, 
+  {
+    path: 'noticias-constructora',
+    loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule)
+  },
+  {
+    path: 'proyectos-construccion-vivienda',
+    loadChildren: () => import('./modules/projects/vivienda/vivienda.module').then(m => m.ViviendaModule)
+  },
+  {
+    path: 'proyectos-construccion-comerciales-industriales',
+    loadChildren: () => import('./modules/projects/comerciales/comerciales.module').then( m => m.ComercialesModule )
+  },
+  {
+    path: 'proyecto/:slug',
+    loadChildren: () => import('./modules/projects/interna/interna.module').then(m => m.InternaModule)
+  },
+  {
+    path: 'legales',
+    loadChildren: () => import('./modules/legales/legales.module').then( m => m.LegalesModule )
+  },{
+    path: 'proyectos-ejecutados',
+    loadChildren: () => import('./modules/projects/ejecutados/ejecutados.module').then( m => m.EjecutadosModule )
+  },{
+    path: 'pagos-constructora',
+    loadChildren: () => import('./modules/pagos/pagos.module').then( m => m.PagosModule )
+  },{
+    path: 'mapa-del-sitio',
+    loadChildren: () => import('./modules/mapa/mapa.module').then( m => m.MapaModule )
+  },{
+    path: '**',
+    loadComponent: () => import('../app/shared/pages/not-found-page/not-found-page.component').then( m => m.NotFoundPageComponent),
+  }, 
+  {
+    path: '404',
+    loadComponent: () => import('../app/shared/pages/not-found-page/not-found-page.component').then( m => m.NotFoundPageComponent),
+  }
 ];
 
 @NgModule({
