00e49511c2be4d345445a1f62e7242266fde3b89
Jeison Obando <jeison.obando@paxzu.co>
2024-07-04 16:27:12 -0500

    delete interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
-import { HttpInterceptorService } from './core/interceptors/http-interceptor.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -23,11 +22,6 @@ import { APP_BASE_HREF } from '@angular/common';
   providers: [
     [{ provide: APP_BASE_HREF, useValue: '/constructora' }],
     provideHttpClient(withFetch()),
-    // {
-    //   provide: HTTP_INTERCEPTORS,
-    //   useClass: HttpInterceptorService,
-    //   multi: true
-    // },
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
