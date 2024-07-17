099a4eb9a0e30ac9a0fa65d5e4812ae1dd111c3c
Jeison Obando <jeison.obando@paxzu.co>
2024-07-17 09:49:38 -0500

    update envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  base_url: 'https://www.oikos.com.co/constructora/adminpaxzu/',
-  api_url: 'https://www.oikos.com.co/constructora/adminpaxzu/api/', //establecer el subdominio para esta sección
+  base_url: 'https://adminpaxzu-constructora.oikos.com.co/',
+  api_url: 'https://adminpaxzu-constructora.oikos.com.co/api/', //establecer el subdominio para esta sección
   token: 'aff7d877-d0ff-4dfa-a85c-d9715ea82061', //se genera al publicar,
   token_email: 'c2424d96-2da4-486b-a384-902d1a7b8536',
-  imagenes_url: 'https://www.oikos.com.co/constructora/', //establecer el subdominio para esta sección
+  imagenes_url: 'https://adminpaxzu-constructora.oikos.com.co/', //establecer el subdominio para esta sección
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  base_url: 'https://www.oikos.com.co/constructora/adminpaxzu/',
-  api_url: 'https://www.oikos.com.co/constructora/adminpaxzu/api/', //establecer el subdominio para esta sección
+  production: true,
+  base_url: 'https://adminpaxzu-constructora.oikos.com.co/',
+  api_url: 'https://adminpaxzu-constructora.oikos.com.co/api/', //establecer el subdominio para esta sección
   token: 'aff7d877-d0ff-4dfa-a85c-d9715ea82061', //se genera al publicar,
   token_email: 'c2424d96-2da4-486b-a384-902d1a7b8536',
-  imagenes_url: 'https://www.oikos.com.co/constructora/', //establecer el subdominio para esta sección
+  imagenes_url: 'https://adminpaxzu-constructora.oikos.com.co/', //establecer el subdominio para esta sección
 };
 
 
