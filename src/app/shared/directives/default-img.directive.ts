c134c16d0a4fc0d8045ceb1b182cea6bda0da044
Jeison Obando <jeison.obando@paxzu.co>
2024-07-18 16:54:06 -0500

    def img

--- src/app/shared/directives/default-img.directive.ts
+++ src/app/shared/directives/default-img.directive.ts
@@ -4,16 +4,16 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[defaultImg]',
   standalone: true
 })
-export class DefaultImgDirective{
+export class DefaultImgDirective {
 
-  @Input() defaultLink:string = 'https://www.oikos.com.co/constructora/images/website/Image_not_available.png';
+  @Input() defaultLink: string = 'https://adminpaxzu-constructora.oikos.com.co/images/website/Image_not_available.png';
 
   constructor(private elementRef: ElementRef) { }
 
   @HostListener('error')
-  loadDefaultImage(){
-    const image:HTMLImageElement = this.elementRef.nativeElement
-    if(image.src === this.defaultLink){
+  loadDefaultImage() {
+    const image: HTMLImageElement = this.elementRef.nativeElement
+    if (image.src === this.defaultLink) {
       console.error(`La imagen por default ${this.defaultLink} está rota, por favor asigna una url válida`)
     } else {
       image.src = this.defaultLink;
